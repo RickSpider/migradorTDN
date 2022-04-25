3066b21ef336ca83ec1b5cf5c3913dcacb55845e
RickSpider <rrgi89@hotmail.com>
2022-04-25 11:27:19 -0400

    clase comprobante id para consultas

---
+++ src/com/migradortdn/model/ComprobanteID.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.migradortdn.model;
+
+/**
+ *
+ * @author BlackSpider
+ */
+public class ComprobanteID {
+    
+    Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+    
+}
